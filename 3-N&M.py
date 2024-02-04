52ebdd6ef6897fae82ed4d2c7fb112822d0f0b73
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-02-04 18:36:54 +0100

    Update 3-N&M.py

--- 3-N&M.py
+++ 3-N&M.py
@@ -1,23 +1,69 @@
+# Alle tal er int heruder:
+# Dette printer en string som fortæller den vil tælle hønner
 print("I will now count my chickens:")
 
+# Dette er udreningen , så der er en string som siger hens/rooster - så et komma og derefter udregningerne
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 7)
 
+# Dette printer en string som fortæller den vil tælle æg
 print("Now I will count the eggs:")
 
+# Dette printerne udregningen
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# Dette er en string med et spørgsmål, hvor den besvare med et boolian værdi pga. <
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# Dette udregningen, som spørgsmålet stillede, hvor boolian værdien bliver false
 print(3 + 2 < 5 - 7)
 
+# Dette er 2 print med strings, som stiller et spørgsmål
 print("What is 3 + 2?", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
+# Dette er en string som forklarer hvorfor statement er falsk
 print("Oh, that's why it's False.")
 
+# Denne string stiller et spørgsmål
 print("How about some more.")
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+# Dette er 3 strings, med 3 sammenligner med en string som spørgsmål og derefter int som spørger om tallet er mindre eller større
+print("Is it greater?", 5 > -2) # true
+print("Is it greater or equal?", 5 >= -2) # true
+print("Is it less or equal?", 5 <= -2) # false
+
+# Alle tal er float heruder:
+# Dette printer en string som fortæller den vil tælle hønner
+print("I will now count my chickens:")
+
+# Dette er udreningen , så der er en string som siger hens/rooster - så et komma og derefter udregningerne
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 7.0)
+
+# Dette printer en string som fortæller den vil tælle æg
+print("Now I will count the eggs:")
+
+# Dette printerne udregningen
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Dette er en string med et spørgsmål, hvor den besvare med et boolian værdi pga. <
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+# Dette udregningen, som spørgsmålet stillede, hvor boolian værdien bliver false
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Dette er 2 print med strings, som stiller et spørgsmål
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Dette er en string som forklarer hvorfor statement er falsk
+print("Oh, that's why it's False.")
+
+# Denne string stiller et spørgsmål
+print("How about some more.")
+
+# Dette er 3 strings, med 3 sammenligner med en string som spørgsmål og derefter int som spørger om tallet er mindre eller større
+print("Is it greater?", 5.0 > -2.0) # true
+print("Is it greater or equal?", 5.0 >= -2.0) # true
+print("Is it less or equal?", 5.0 <= -2.0) # false
