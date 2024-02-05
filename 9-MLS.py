828bdf26c96f4b897e013052d08d6f261b61d599
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-02-05 22:36:56 +0100

    Create 9-MLS.py

---
+++ 9-MLS.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember to type it exactly.
+# 2 variabler, som indeholder ugedage og måneder, som skifter linje med \n
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# 2 print som indeholder string, der benytter sig af tidligere nævnte variabler
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# Flere linjet string pga. """
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
